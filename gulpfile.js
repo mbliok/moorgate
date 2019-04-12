5c1301a79587918e906a10b47bc98a87d63b0e1c
mbliok <mbliok@melontech.com>
2019-04-12 12:32:02 +0300

    First commit. base set up sass with gulp

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+// 'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+gulp.task('styles', function() {
+  gulp
+    .src('./scss/main.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task('server', function() {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
+
+  gulp.watch('./scss/*.scss', ['styles']);
+  gulp.watch('./**/*.html').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['styles', 'server']);
